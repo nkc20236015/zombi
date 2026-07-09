4926bcce525270bda43fbe0b262391a3303ad4c8
nkc20236015 <134424890+nkc20236015@users.noreply.github.com>
2026-07-09 10:40:40 +0900

    Tool: Update LOD tool to fix LODGroup instead of removing it (which breaks Unity Terrain rendering)

--- Assets/_App/Scripts/Editor/RemoveLODGroupTool.cs
+++ Assets/_App/Scripts/Editor/RemoveLODGroupTool.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 using UnityEditor;
-using System.IO;
 
-public class RemoveLODGroupTool
+public class FixLODGroupTool
 {
-    [MenuItem("Tools/Remove LODGroup from Trees")]
-    public static void RemoveAllLODGroups()
+    [MenuItem("Tools/Fix Tree LODGroups (Prevent Popping)")]
+    public static void FixAllLODGroups()
     {
         string[] searchFolders = new string[] { "Assets" };
         string[] guids = AssetDatabase.FindAssets("t:Prefab", searchFolders);
         
-        int removedCount = 0;
+        int fixedCount = 0;
 
         foreach (string guid in guids)
         {
@@ -26,19 +25,27 @@ public class RemoveLODGroupTool
                 LODGroup lod = prefab.GetComponent<LODGroup>();
                 if (lod != null)
                 {
-                    Object.DestroyImmediate(lod, true);
-                    EditorUtility.SetDirty(prefab);
-                    removedCount++;
-                    Debug.Log($"Removed LODGroup from: {prefab.name}");
+                    LOD[] lods = lod.GetLODs();
+                    if (lods.Length > 0)
+                    {
+                        // 最初のLOD（一番高品質なLOD0）だけを残し、画面にどれだけ小さく映っても消えないようにする
+                        LOD lod0 = lods[0];
+                        lod0.screenRelativeTransitionHeight = 0.001f; // ほぼ0%までLOD0を描画
+
+                        lod.SetLODs(new LOD[] { lod0 });
+                        EditorUtility.SetDirty(prefab);
+                        fixedCount++;
+                        Debug.Log($"Fixed LODGroup for: {prefab.name}");
+                    }
                 }
             }
         }
 
-        if (removedCount > 0)
+        if (fixedCount > 0)
         {
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
-            Debug.Log($"<color=green>LODGroupの削除が完了しました！ 計 {removedCount} 個のプレハブを修正しました。</color>");
+            Debug.Log($"<color=green>LODGroupの修正が完了しました！ 計 {fixedCount} 個のプレハブを修正し、サイズ変化を防ぎました。</color>");
         }
         else
         {
